c83e2321402107b91d17b1619789e74187f0c82f
Igor Bobusky <igor.bobusky@gmail.com>
2020-10-14 19:40:53 +0200

    Sprint boot application - creation

---
+++ src/main/java/com/example/chatappdoodle/ChatAppDoodleApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatappdoodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppDoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppDoodleApplication.class, args);
+	}
+
+}
